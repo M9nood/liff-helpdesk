5ebba2b656187c0ec0d6e3d0417fb055e155bb90
Pamorn Trivorrarat <chocobobu@gmail.com>
2022-10-15 18:24:29 +0700

    add parameter withLoginOnExternalBrowser  on liff.init - for user friendly on mobile it will auto login with LINE Application

--- src/mixins/liff-mixin.js
+++ src/mixins/liff-mixin.js
@@ -6,7 +6,7 @@ const liffMixin  = {
   },
   methods : {
     async initLiff(){
-      await this.$liff.init({ liffId: process.env.LIFF_ID })
+      await this.$liff.init({ liffId: process.env.LIFF_ID, withLoginOnExternalBrowser: true })
       if (this.$liff.isInClient() && !localStorage.getItem('li_user') && this.$router.currentRoute.path != '/login') {
         // this.$router.push({
         //   path : '/login'
